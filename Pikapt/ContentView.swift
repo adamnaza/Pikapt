ca5107cd318a06488db4ef72721e9cd642351a96
adamnaza <adamchenaza@gmail.com>
2025-11-16 02:47:26 -0500

    Initial Commit

---
+++ Pikapt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pikapt
+//
+//  Created by Adam Che Nazahatuhisamudin on 11/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pikapt/PikaptApp.swift
@@ -0,0 +1,17 @@
+//
+//  PikaptApp.swift
+//  Pikapt
+//
+//  Created by Adam Che Nazahatuhisamudin on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct PikaptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
