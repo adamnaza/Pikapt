7c49f30a895c63e501c55792f714fc0f648d10c5
adamnaza <adamchenaza@gmail.com>
2025-11-30 21:36:54 -0500

    added changes to the constants and HomeView tab

--- Pikapt/ContentView.swift
+++ Pikapt/ContentView.swift
@@ -10,19 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView{
-            Tab("Constants.homeString", systemImage: Constants.homeIconString){
-                Text("Constants.homeString")
+            Tab(Constants.homeString, systemImage: Constants.homeIconString){
+                HomeView()
                 
             }
-            Tab("Constants.upcomingString", systemImage: Constants.upcomingIconString){
+            Tab(Constants.upcomingString, systemImage: Constants.upcomingIconString){
                 Text("Constants.upcomingString")
                 
             }
-            Tab("Constants.searchString", systemImage: Constants.searchIconString){
+            Tab(Constants.searchString, systemImage: Constants.searchIconString){
                 Text("Constants.searchString")
                 
             }
-            Tab("Constants.downloadString", systemImage: Constants.downloadIconString){
+            Tab(Constants.downloadString, systemImage: Constants.downloadIconString){
                 Text("Constants.downloadString")
                 
             }
